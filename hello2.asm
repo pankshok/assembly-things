eadae1ef51975678c569dc7d2a59bacdb836c6d5
pkulev <pkulev@croc.ru>
2015-07-27 12:06:10 +0300

    add hello2.asm

---
+++ hello2.asm
@@ -0,0 +1,24 @@
+;-------------+
+; Hello world ;
+;-------------+
+
+global _start
+
+section .DATA
+msg:
+    db  "Hello world!", 10
+
+
+section .TEXT
+
+_start:
+    mov rax, 1    ; 1 is write syscall
+    mov rdi, 1    ; 1 is stdout file handler
+    mov rsi, msg  ; strings' address
+    mov rdx, 13   ; number of bytes
+    syscall
+
+    mov eax, 60   ; 60 is exit syscall
+    xor rdi, rdi  ; exit code 0 ?!?!?!
+    syscall
+
