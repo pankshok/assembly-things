1adac10a7ecf19d4559c336fcd05e8f4b162a74c
pkulev <pkulev@croc.ru>
2015-07-27 22:41:50 +0300

    add gitignore and another funny hullowrdl %)

--- hello2.asm
+++ hello2.asm
@@ -12,13 +12,13 @@ msg:
 section .TEXT
 
 _start:
-    mov rax, 1    ; 1 is write syscall
-    mov rdi, 1    ; 1 is stdout file handler
-    mov rsi, msg  ; strings' address
-    mov rdx, 13   ; number of bytes
+    mov     rax, 1      ; 1 is write syscall
+    mov     rdi, 1      ; 1 is stdout file handler
+    mov     rsi, msg    ; strings' address
+    mov     rdx, 13     ; number of bytes
     syscall
 
-    mov eax, 60   ; 60 is exit syscall
-    xor rdi, rdi  ; exit code 0 ?!?!?!
+    mov     eax, 60     ; 60 is exit syscall
+    xor     rdi, rdi    ; exit code 0 ?!?!?!
     syscall
 
